bae72523ad9c70f6d4737ca5e6411dd479cd115f
Anguinao, Leonardo <languiano@csus.edu>
2023-12-28 16:40:45 -0800

    creating a form and setting validation

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,25 +4,12 @@ import produce from "immer";
 import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
 import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos qui
-        voluptatem, dolorem nam quas repellat laboriosam fuga eveniet at magni
-        ducimus. Voluptas provident autem veritatis nam beatae, enim debitis
-        laboriosam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque laborum, error officia magni ratione totam vel! Accusantium,
-        sequi quis asperiores, nihil et porro officiis obcaecati rem esse nam,
-        dolore non. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        Natus eaque pariatur dolore esse, enim similique ea voluptatibus
-        delectus rerum modi reprehenderit, eos officia, est repellendus a. Quasi
-        repellendus minus numquam. Lorem, ipsum dolor sit amet consectetur
-        adipisicing elit. Incidunt repellendus sequi vitae provident tenetur
-        perferendis minima magnam eum voluptate id neque maiores, expedita
-        repellat? Incidunt, vel autem? Delectus, tempora eius?
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
