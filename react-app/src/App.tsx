64fa0e807e37cecfea58d54ab6745aa5f8a2dd73
Anguinao, Leonardo <languiano@csus.edu>
2024-01-12 15:49:20 -0800

    expense table created

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -5,11 +5,36 @@ import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
 import ExpandableText from "./components/ExpandableText";
 import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    {
+      id: 1,
+      description: "Groceries",
+      amount: 80,
+      category: "Food",
+    },
+    {
+      id: 2,
+      description: "Gas",
+      amount: 40,
+      category: "Car",
+    },
+    {
+      id: 3,
+      description: "Rent",
+      amount: 1000,
+      category: "Bills",
+    },
+  ]);
+
   return (
     <div>
-      <Form />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </div>
   );
 }
